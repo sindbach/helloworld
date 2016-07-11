289a62bec9aaa6b6db72ee1f8e713e2fc029ff42
Wan Bachtiar <sindbach@gmail.com>
2016-07-11 12:50:48 +1000

    Fix scheduler

--- app.py
+++ app.py
@@ -4,23 +4,22 @@ from os import environ
 
 from flask import Flask
 from flask import render_template
-from apscheduler.schedulers import Scheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 app = Flask(__name__)
 
-cron = Scheduler(daemon=True)
-cron.start()
-
+sched = BlockingScheduler()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@cron.interval_schedule(minutes=3)
+@sched.scheduled_job('cron', id='scheduled', minutes=3)
 def scheduled_worker():
     print "Interval schedule being called"
 
-atexit.register(lambda: cron.shutdown(wait=False))
+sched.start()
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=80)
