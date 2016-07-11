52dfcd2ed9b29aca7d6f766152de6e7c7a88c5df
Wan Bachtiar <sindbach@gmail.com>
2016-07-11 12:02:34 +1000

    Add something in index.html

--- app.py
+++ app.py
@@ -5,14 +5,11 @@ from os import environ
 from flask import Flask
 from flask import render_template
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
-    return render_template('index.html',
-                           powered_by=environ.get('POWERED_BY', 'Deis'))
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
