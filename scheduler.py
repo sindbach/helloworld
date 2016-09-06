f93c2e21d110f8984e3dc1002afcb740dd9d4a92
Wan Bachtiar <sindbach@gmail.com>
2016-09-06 19:28:12 +1000

    Replace flask.redirect with urllib.request

--- scheduler.py
+++ scheduler.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import pytz
 import logging
+import urllib.request
 logging.basicConfig()
 from apscheduler.schedulers.blocking import BlockingScheduler
-from flask import redirect
 sched = BlockingScheduler(daemon=True)
 
 
 if __name__ == "__main__":
-    sched.add_job(func=redirect("/worka"), trigger='interval', id='scheduled_workerA', max_instances=1, minutes=3, timezone=pytz.utc)
-    sched.add_job(func=redirect("/workb"), trigger='interval', id='scheduled_workerB', max_instances=1, minutes=5, timezone=pytz.utc)
+    sched.add_job(func=urllib.request.urlopen("/worka"), trigger='interval', id='scheduled_workerA', max_instances=1, minutes=3, timezone=pytz.utc)
+    sched.add_job(func=urllib.request.urlopen("/workb"), trigger='interval', id='scheduled_workerB', max_instances=1, minutes=5, timezone=pytz.utc)
     sched.start()
