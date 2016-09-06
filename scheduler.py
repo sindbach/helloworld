aceb7026ec8c0fb28efb64854f852b56a85836aa
Wan Bachtiar <sindbach@gmail.com>
2016-09-07 09:21:08 +1000

    Replace func to be callable

--- scheduler.py
+++ scheduler.py
@@ -8,11 +8,17 @@ sched = BlockingScheduler(daemon=True)
 
 HOME_URL = "http://nonfat-zoologer.mongodb.cc/"
 
-def trigger_worker(path):
+def trigger_workerA():
+    path = "worka"
+    grequests.map([grequests.get(HOME_URL+path)])
+    return 
+
+def trigger_workerB():
+    path = "workb"
     grequests.map([grequests.get(HOME_URL+path)])
     return 
 
 if __name__ == "__main__":
-    sched.add_job(func=trigger_worker("worka"), trigger='interval', id='scheduled_workerA', max_instances=1, minutes=3, timezone=pytz.utc)
-    sched.add_job(func=trigger_worker("workb"), trigger='interval', id='scheduled_workerB', max_instances=1, minutes=5, timezone=pytz.utc)
+    sched.add_job(func=trigger_workerA, trigger='interval', id='scheduled_workerA', max_instances=1, minutes=3, timezone=pytz.utc)
+    sched.add_job(func=trigger_workerB, trigger='interval', id='scheduled_workerB', max_instances=1, minutes=5, timezone=pytz.utc)
     sched.start()
