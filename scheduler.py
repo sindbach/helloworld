4da2a637c27d21c812fe517357d569642b1658b4
Wan Bachtiar <sindbach@gmail.com>
2016-09-06 19:34:09 +1000

    Replace urllib with requests

--- scheduler.py
+++ scheduler.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import pytz
 import logging
-import urllib.request
+import requests
 logging.basicConfig()
 from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler(daemon=True)
 
 
 if __name__ == "__main__":
-    sched.add_job(func=urllib.request.urlopen("/worka"), trigger='interval', id='scheduled_workerA', max_instances=1, minutes=3, timezone=pytz.utc)
-    sched.add_job(func=urllib.request.urlopen("/workb"), trigger='interval', id='scheduled_workerB', max_instances=1, minutes=5, timezone=pytz.utc)
+    sched.add_job(func=requests.get("/worka"), trigger='interval', id='scheduled_workerA', max_instances=1, minutes=3, timezone=pytz.utc)
+    sched.add_job(func=requests.get("/workb"), trigger='interval', id='scheduled_workerB', max_instances=1, minutes=5, timezone=pytz.utc)
     sched.start()
