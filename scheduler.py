e99543aeb1ec2991d7789815e43877ab5afb007e
Wan Bachtiar <sindbach@gmail.com>
2016-09-06 19:38:53 +1000

    Specify full url

--- scheduler.py
+++ scheduler.py
@@ -8,6 +8,6 @@ sched = BlockingScheduler(daemon=True)
 
 
 if __name__ == "__main__":
-    sched.add_job(func=requests.get("/worka"), trigger='interval', id='scheduled_workerA', max_instances=1, minutes=3, timezone=pytz.utc)
-    sched.add_job(func=requests.get("/workb"), trigger='interval', id='scheduled_workerB', max_instances=1, minutes=5, timezone=pytz.utc)
+    sched.add_job(func=requests.get("http://nonfat-zoologer.mongodb.cc/worka"), trigger='interval', id='scheduled_workerA', max_instances=1, minutes=3, timezone=pytz.utc)
+    sched.add_job(func=requests.get("http://nonfat-zoologer.mongodb.cc/workb"), trigger='interval', id='scheduled_workerB', max_instances=1, minutes=5, timezone=pytz.utc)
     sched.start()
